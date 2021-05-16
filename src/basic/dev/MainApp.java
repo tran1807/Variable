bd0c6fc5d71765ae7d316d52b410cb736355a282
github <nhutranle59@gmail.com>
2021-05-16 08:15:29 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,19 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String firstName= "Phan Van";
+		String lastName= "Nam";
+		int old= 15;
+		float markMath= 9f;
+		boolean passed= true;
+		
+		System.out.println("Ho va ten : " + firstName+ " "+ lastName);
+		System.out.println("Mark Math: "+ markMath);
+		System.out.println("Passed: "+ passed);
+		
+	}
+
+}
